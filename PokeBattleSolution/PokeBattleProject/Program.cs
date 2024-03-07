9c0db388b2fb1d9484c20673ddae531d1b310e8a
t1mmyboy <99074833@mydavinci.nl>
2024-03-07 11:39:38 +0100

    done with 4

--- PokeBattleSolution/PokeBattleProject/Program.cs
+++ PokeBattleSolution/PokeBattleProject/Program.cs
@@ -1,127 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Xml.Linq;
-/*
-Console.WriteLine("Pokemon Battle Simulator");
-var trainer1 = new Trainer("Henk");
-var trainer2 = new Trainer("Bob");
-trainer1.takePokeball(new Pokeball(new Charmander("blaze1")));
-trainer1.takePokeball(new Pokeball(new Charmander("blaze2")));
-trainer1.takePokeball(new Pokeball(new Charmander("blaze3")));
-trainer1.takePokeball(new Pokeball(new Charmander("blaze4")));
-trainer1.takePokeball(new Pokeball(new Charmander("blaze5")));
-trainer1.takePokeball(new Pokeball(new Charmander("blaze6")));
-
-trainer2.takePokeball(new Pokeball(new Charmander("steve1")));
-trainer2.takePokeball(new Pokeball(new Charmander("steve2")));
-trainer2.takePokeball(new Pokeball(new Charmander("steve3")));
-trainer2.takePokeball(new Pokeball(new Charmander("steve4")));
-trainer2.takePokeball(new Pokeball(new Charmander("steve5")));
-trainer2.takePokeball(new Pokeball(new Charmander("steve6")));
-while (true)
-{
-    for (int x = 0; x < trainer1.belt.Count; x++)
-    {
-        var charm2 = trainer1.throwPokeball();
-        trainer1.returnPokemon(charm2);
-
-        var charm3 = trainer2.throwPokeball();
-        trainer2.returnPokemon(charm3);
-    }
-    Console.WriteLine("Do you want to play again?");
-    string answer = Console.ReadLine();
-    if (answer == "no" || answer == "No")
-    {
-        break;
-    }
-    else { trainer1.teller = 0;
-        trainer2.teller = 0;
-    }
-}
-
-
-public class Charmander
-{
-    public string? nickname;
-    public string strenght;
-    public string weakness;
-    public Charmander(string newNickname)
-    {
-        this.nickname = newNickname;
-        this.strenght = "fire";
-        this.weakness = "water";
-    }
-
-    public void battleCry()
-    {
-        Console.WriteLine(getName().ToUpper());
-    }
-    public string getName()
-    {
-        return this.nickname;
-    }
-}
-
-
-public class Pokeball
-{
-    public Charmander? Charmander;
-    public bool hasCharmanderInside;
-
-    public Pokeball(Charmander Charmander)
-    {
-        this.Charmander = Charmander;
-        this.hasCharmanderInside = true;
-
-    }
-    public Charmander Open()
-    {
-        hasCharmanderInside = false;
-        return Charmander;
-    }
-    public void Close()
-    {
-        hasCharmanderInside = true;
-    }
-
-
-}
-
-public class Trainer
-{
-    public List<Pokeball> belt;
-    public string? name;
-    public int teller = 0;
-
-    public Trainer(string name)
-    {
-        this.belt = new List<Pokeball>();
-        this.name = name;
-    }
-
-    public void takePokeball(Pokeball pokeball)
-    {
-        // check for maximum amount of pokeballs
-        if (belt.Count >= 6) { Console.WriteLine(belt.Count); throw new Exception("Kan niet meer dan 6 Pokemons hebben op uw belt!");}
-        else { belt.Add(pokeball); }
-    }
-    public Charmander throwPokeball()
-    {
-        Charmander charm1 = belt[teller].Open();
-        Console.WriteLine(name+"'s Charmander says: " + charm1.getName());
-        teller = teller + 1;
-        return charm1;
-    }
-    public void returnPokemon(Charmander charmander)
-    {
-        
-        Console.WriteLine(name + " Returns: " + charmander.getName());
-    }
-}
-*/
-
-
-
-
 while (true)
 {
     Console.WriteLine("Pokemon battle simulator");
@@ -147,17 +25,13 @@ while (true)
     trainer1.takePokeball(new Pokeball(new Squirtle("Squirtle")));
     trainer2.takePokeball(new Pokeball(new Squirtle("Squirtle2")));
 
-    for (int i = 0; i < 6; i++)
-    {
-        trainer1.teller = i;
-        Pokemon pokemon = trainer1.throwPokeball();
-        trainer1.returnPokemon(pokemon);
+    Battle battle = new Battle(trainer1, trainer2);
+    battle.randomizer();
 
-        trainer2.teller = i;
-        Pokemon pokemon2 = trainer2.throwPokeball();
-        trainer2.returnPokemon(pokemon2);
+    int[] winners = battle.pokemonChecker();
+    battle.winnerChecker(winners);
 
-    }
+    
 
     Console.WriteLine("Do you wish to play again?");
     string answer = Console.ReadLine();
@@ -296,4 +170,89 @@ public class Trainer
         belt[teller].Close(pokemon);
     }
 }
-    
+public class Battle
+{
+    public static int trainer1wins;
+    public static int trainer2wins;
+    public Trainer? trainer1;
+    public Trainer? trainer2;
+    public static int round;
+
+    public Battle (Trainer trainer1, Trainer trainer2)
+    {
+        this.trainer1 = trainer1;
+        this.trainer2 = trainer2;
+        trainer1wins = 0;
+        trainer2wins = 0;
+        round = 0;
+
+        
+    }
+    public void randomizer()
+    {
+        Console.WriteLine("Randomizing belts...");
+        Random rand = new Random();
+        Random rand2 = new Random();
+        trainer1.belt = trainer1.belt.OrderBy(_ => rand.Next()).ToList();
+        trainer2.belt = trainer2.belt.OrderBy(_ => rand.Next()).ToList();
+
+       
+    }
+    public int[] pokemonChecker()
+    {
+        
+        while (true)
+        {
+            if (trainer1wins > 5 || trainer2wins > 5)
+            {
+                break;
+            }
+            
+            Console.WriteLine("Round: " + round+1);
+            trainer1.teller = trainer2wins;
+            trainer2.teller = trainer1wins;
+            Pokemon pokemon1 = trainer1.throwPokeball();
+            Pokemon pokemon2 = trainer2.throwPokeball();
+            
+            
+            if (pokemon1.strenght == pokemon2.weakness)
+            {
+                trainer1wins++;
+                Console.WriteLine(trainer1.name+ "'s "+ pokemon1.nickname + " Defeats " + trainer2.name +"'s " +pokemon2.nickname);
+            }
+            else if (pokemon1.strenght == pokemon2.strenght)
+            {
+                trainer1wins++;
+                trainer2wins++;
+                Console.WriteLine(trainer1.name + "'s " + pokemon1.nickname + " Draws against " + trainer2.name + "'s " + pokemon2.nickname);
+            }
+            else if (pokemon1.weakness == pokemon2.strenght)
+            {
+                trainer2wins++;
+                Console.WriteLine(trainer1.name + "'s " + pokemon1.nickname + " Loses against " + trainer2.name + "'s " + pokemon2.nickname);
+            }
+            Console.WriteLine(trainer1.name + ": " + trainer1wins + " " + trainer2.name + ": " + trainer2wins);
+            round++;
+            trainer1.returnPokemon(pokemon1);
+            trainer2.returnPokemon(pokemon2);
+            
+        }
+        int[] winners = { trainer1wins, trainer2wins };
+        return winners;
+    }
+    public void winnerChecker(int[] winner)
+    {
+        if (winner[0] > winner[1])
+        {
+            Console.WriteLine(trainer1.name + " Wins!");
+        }
+        else if (winner[1] > winner[0])
+        {
+            Console.WriteLine(trainer2.name + " Wins!");
+        }
+        else if (winner[0] == winner[1])
+        {
+            Console.WriteLine(trainer1.name +" and "+trainer2.name+ " Draw!");
+        }
+    }
+}
